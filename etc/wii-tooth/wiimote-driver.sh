d182e8a846db08987d7f9b1c381e5e15d222565b
Thomas Castleman <thomas.castleman2019@gmail.com>
2019-02-16 18:23:20 -0500

    Bug fixes
    code should now actually be working properly.

---
+++ etc/wii-tooth/wiimote-driver.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# -*- coding: utf-8 -*-
+#
+#  wiimote-driver.sh
+#  
+#  Copyright 2019 Thomas Castleman <contact@draugeros.ml>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#
+x="0"
+while true; do
+	if [ ! -f /etc/wii-tooth/check.flag ] && [ "$x" == "1" ]; then
+		continue
+	else
+		/bin/rm /etc/wii-tooth/check.flag
+	fi
+	/etc/wii-tooth/check.sh &
+	/bin/sleep 6s
+	if [ ! -f /etc/wii-tooth/check.flag ]; then
+		/usr/bin/notify-send "Wiimote Connected!"
+		x="1"
+		/bin/sleep 10s
+	else
+		continue
+		x="0"
+	fi
+done
+
