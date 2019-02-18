140e770445fe0f26acc336301ac44913cd2147b8
Thomas Castleman <thomas.castleman2019@gmail.com>
2019-02-18 16:27:37 -0500

    bug fixes
    Moved the Wiimote mapping conf file to /etc/wii-tooth
    Fixed file path in /etc/wii-tooth/check.sh
    Fixed folder permissions for /etc/wii-tooth
    Fixed command file-path issue in /bin/wii-tooth

--- etc/wii-tooth/check.sh
+++ etc/wii-tooth/check.sh
@@ -20,5 +20,5 @@
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #  MA 02110-1301, USA.
 #
-/usr/bin/wminput -r -q -c /home/batcastle/Dropbox/GitHub/wii-tooth/etc/cwiid/wminput/wii-tooth-default.conf >/dev/null 2>/dev/null
+/usr/bin/wminput -r -q -c /etc/wii-tooth/wii-tooth-default.conf >/dev/null 2>/dev/null
 /bin/echo "" > /etc/wii-tooth/check.flag
